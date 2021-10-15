e5623270f5c8021d4851a95599d53580f7d4a8a0
Tim Dugan <tadugan@me.com>
2021-10-14 22:30:21 -0500

    List of notes updates after you add a new note

--- src/redux/sagas/createNote.saga.js
+++ src/redux/sagas/createNote.saga.js
@@ -1,9 +1,10 @@
-import { takeLatest } from '@redux-saga/core/effects';
+import { put, takeLatest } from '@redux-saga/core/effects';
 import axios from 'axios';
 
 function* submitNewNote(action) {
     try {
         yield axios.post('/notes', action.payload);
+        yield put({ type: 'GET_ALL_NOTES' });
     } catch (error) {
         console.log(`error POSTing new note: ${error}`);
     }
